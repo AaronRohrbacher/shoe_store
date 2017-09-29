7e586a185b0014b5fac00579a8eda95179613606
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-29 11:16:07 -0700

    allow creation of brands with prices

--- app.rb
+++ app.rb
@@ -5,29 +5,50 @@ require('pry')
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
-  @stores = Store.all
   erb(:index)
 end
 
-post('/') do
+get('/store') do
+  @stores = Store.all
+  erb(:store)
+end
+
+post('/store') do
   Store.create(store: params['store'])
   @stores = Store.all
-  erb(:index)
+  erb(:store)
 end
 
 get('/store/:id') do
   @store = Store.find(params['id'])
-  erb(:store)
+  erb(:store_edit)
 end
 
 post('/store/:id') do
   @store = Store.find(params['id'])
   @store.update(store: params['store'])
-  erb(:store)
+  erb(:store_edit)
 end
 
 delete('/store/:id') do
   @store = Store.find(params['id'])
   @store.destroy()
   redirect('/')
+end
+
+get('/brand') do
+  @brands = Brand.all
+  erb(:brand)
+end
+
+post('/brand') do
+  Brand.create(brand: params['brand'], price: params['price'])
+  @brands = Brand.all
+  erb(:brand)
+end
+
+get('/brand/:id') do
+  @brand = Brand.find(params['id'])
+  @brand_stores = @brand.stores
+  erb(:brand_edit)
 end
